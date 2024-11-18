efcb5296507a3b9094aa4b4222a857afbdfe4a11
Nahuel-xpag <agustin.nahuu@gmail.com>
2024-11-18 21:00:29 +0000

    feat: css added

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -18,5 +18,9 @@ const messages = [
 indexRouter.get("/", (req, res) => {
     res.render("index", {title: "Mini Messageboard", messages: messages})
 })
+indexRouter.post("/new", (req, res) => {
+    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+    res.redirect("/");
+})
 
 module.exports = indexRouter;
